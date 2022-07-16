7f873c843e2e49b160108c587c1fdce312965be4
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-07-16 20:15:18 +0200

    stuff

--- src/theme/CodeBlock/index.tsx
+++ src/theme/CodeBlock/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import CodeBlock, { type Props } from '@theme-init/CodeBlock';
+//import './styles.css';
+const css = require('./styles.css');
+
 
 export default function CodeBlockWrapper(props: Props) {
   return (
-    <>
-      DEVBOOK PLUGINmxd
+    <div className="dbk-wrapper">
+      <div className="dbk-feedback-btns">
+        <span>Devbook</span>
+        <span>Devbook</span>
+        <span>Devbook</span>
+      </div>
       <CodeBlock {...props} />
-    </>
+    </div>
   );
 }
